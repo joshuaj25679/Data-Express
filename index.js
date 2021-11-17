e540af971f2da77a3b7c6292fad2c514e5370719
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2021-11-17 08:37:05 -0700

    Fixed it again

---
+++ index.js
@@ -0,0 +1,25 @@
+const { urlencoded } = require('express');
+const express = require('express'), 
+    pug = require('pug'),
+    path = require('path'),
+    routes = require('./routes/routes.js');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, '/public')));
+
+const urlencodedParser = express.urlencoded({
+    extended: false
+})
+
+app.get('/', routes.index);
+app.get('/create', routes.create);
+app.post('/create', urlencodedParser, routes.createPerson);
+app.get('/edit/:id', routes.edit);
+app.post('/edit/:id', urlencodedParser, routes.editPerson);
+app.get('/delete/:id', routes.delete);
+app.get('/details/:id', routes.details);
+
+app.listen(3000);
