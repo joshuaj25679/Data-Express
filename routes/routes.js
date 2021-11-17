3bd85214a0e60fd699d5c75619251a7abeecb542
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2021-11-17 08:42:40 -0700

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const {MongoClient, ObjectId} = require("mongodb");
 
-const url = 'mongodb+srv://jojohnson:republic1776@cluster0.4tguq.mongodb.net/DataExpress?retryWrites=true&w=majority';
+const url = 'mongodb+srv://teammates:hello@cluster0.4tguq.mongodb.net/DataExpress?retryWrites=true&w=majority';
 const client = new MongoClient(url);
 
 const dbName = 'DataExpress';
