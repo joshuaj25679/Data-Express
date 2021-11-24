f695f01a8cfac713cf3ea3151f7877cdc06390ef
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2021-11-24 08:47:09 -0700

    Fixed Stuff

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,4 @@
-const {MongoClient, ObjectId} = require("mongodb"),
+const { MongoClient, ObjectId } = require("mongodb"),
     bcrypt = require('bcryptjs');
 
 const url = 'mongodb+srv://teammates:hello@cluster0.4tguq.mongodb.net/DataExpress?retryWrites=true&w=majority';
@@ -21,50 +21,57 @@ exports.login = (req, res) => {
 exports.loginUser = async (req, res) => {
     console.log(req.body.username);
     await client.connect();
-    const filteredDocs = await collection.findOne({'username': req.body.username});
+    const filteredDocs = await collection.findOne({ 'username': req.body.username });
     console.log(filteredDocs);
     client.close();
-    //Change to pull from Database Username
-    if(req.body.username == filteredDocs.username && bcrypt.compareSync(req.body.password, filteredDocs.password)){
-        req.session.user = {
-            isAuthenticated: true,
-            username: req.body.username
-        }
-        //if they have been to the site before it will do this
-        if(req.cookies.beenToSiteBefore == 'yes'){
-            //nice format for time
-            s = (Date.now() - req.cookies.LastVisited)
-            var ms = s % 1000;
-            s = (s - ms) / 1000;
-            var secs = s % 60;
-            s = (s - secs) / 60;
-            var mins = s % 60;
-            var hrs = (s - mins) / 60;
-            niceTime = hrs + ':' + mins + ':' + secs + '.' + ms;
+    if(filteredDocs != null){
+        if (req.body.username == filteredDocs.username && bcrypt.compareSync(req.body.password, filteredDocs.password)) {
+            req.session.user = {
+                isAuthenticated: true,
+                username: req.body.username
+            }
+            //if they have been to the site before it will do this
+            if (req.cookies.beenToSiteBefore == 'yes') {
+                //nice format for time
+                s = (Date.now() - req.cookies.LastVisited)
+                var ms = s % 1000;
+                s = (s - ms) / 1000;
+                var secs = s % 60;
+                s = (s - secs) / 60;
+                var mins = s % 60;
+                var hrs = (s - mins) / 60;
+                niceTime = hrs + ':' + mins + ':' + secs + '.' + ms;
 
-            //rewrites the last visited cookie to have this as the new last visited time
-            res.cookie('LastVisited', Date.now(), {maxAge: 9999999999999999999})
+                //rewrites the last visited cookie to have this as the new last visited time
+                res.cookie('LastVisited', Date.now(), { maxAge: 9999999999999999999 })
+            }
+            //if its the first time visiting it will make these cookies
+            else {
+                res.cookie('beenToSiteBefore', 'yes', { maxAge: 9999999999999999999 });
+                res.cookie('LastVisited', Date.now(), { maxAge: 9999999999999999999 });
+                niceTime = "First Time Here!"
+            }
+            res.render(`details`, {
+                lastVisit: niceTime,
+                person: filteredDocs
+            })
         }
-        //if its the first time visiting it will make these cookies
-        else{
-            res.cookie('beenToSiteBefore', 'yes', {maxAge: 9999999999999999999});
-            res.cookie('LastVisited', Date.now(), {maxAge: 9999999999999999999});
-            niceTime = "First Time Here!"
-        }
-        res.render(`details`, {
-            lastVisit: niceTime,
-            person: filteredDocs
-        })
+        else {
+            res.redirect('/');
+        };
     }
     else {
-        res.redirect('/');
-    };
+        res.render('loginPlaceHolder', {
+            title: "Login",
+            errorMessage: "Invalid Username or Password"
+        });
+    }
 };
 
 //Logout Method
 exports.logout = (req, res) => {
     req.session.destroy(err => {
-        if(err){
+        if (err) {
             console.log(err);
         }
         else {
@@ -87,11 +94,11 @@ exports.createPerson = async (req, res) => {
     let ans1 = req.body.q1Answers;
     let ans2 = req.body.q2Answers;
     let ans3 = req.body.q3Answers;
-    if(ans1 == undefined || ans2 == undefined || ans3 == undefined){
+    if (ans1 == undefined || ans2 == undefined || ans3 == undefined) {
         res.redirect('/create');
     }
     let password = req.body.password;
-    if(password == ''){
+    if (password == '') {
         res.redirect('/create');
     }
     let username = req.body.username;
@@ -100,7 +107,7 @@ exports.createPerson = async (req, res) => {
     let email = req.body.email;
 
     //If Registration Fields are blank, redirect back to registration page
-    if(username == '' || age == '' || email == ''){
+    if (username == '' || age == '' || email == '') {
         res.redirect('/create');
     }
 
@@ -124,29 +131,29 @@ exports.createPerson = async (req, res) => {
 }
 
 //User Edit Page
-exports.edit = async (req, res) =>{
+exports.edit = async (req, res) => {
     await client.connect();
     //TODO Change to take in username
-    const filterDocs = await collection.find({'username':req.params.username}).toArray();
+    const filterDocs = await collection.find({ 'username': req.params.username }).toArray();
     client.close();
     res.render('edit', {
-        title:'Edit Profile',
+        title: 'Edit Profile',
         person: filterDocs[0]
     });
 };
 
 //Allow User to edit their details
-exports.editPerson = async (req, res) =>{
+exports.editPerson = async (req, res) => {
 
     let ans1 = req.body.ans1;
     let ans2 = req.body.ans2;
     let ans3 = req.body.ans3;
     console.log(ans1, ans2, ans3);
-    if(ans1 == undefined || ans2 == undefined || ans3 == undefined){
+    if (ans1 == undefined || ans2 == undefined || ans3 == undefined) {
         res.redirect('/edit/' + req.session.user.username);
     }
     let password = req.body.password;
-    if(password == ''){
+    if (password == '') {
         res.redirect('/edit/' + req.session.user.username);
     }
     let username = req.body.username;
@@ -154,35 +161,37 @@ exports.editPerson = async (req, res) =>{
     let age = req.body.age;
     let email = req.body.email;
 
-    if(username == '' || age == '' || email == ''){
+    if (username == '' || age == '' || email == '') {
         res.redirect('/edit/' + req.session.user.username);
     }
 
     await client.connect();
     const updateResult = await collection.updateOne(
-        {'username':req.session.user.username},
-        {$set: {
-            username,
-            password,
-            email,
-            age,
-            question1: ans1,
-            question2: ans2,
-            question3: ans3
-            
-        }}
+        { 'username': req.session.user.username },
+        {
+            $set: {
+                username,
+                password,
+                email,
+                age,
+                question1: ans1,
+                question2: ans2,
+                question3: ans3
+
+            }
+        }
     );
     client.close();
     res.redirect('/loggedIn');
 };
 
 //Display User Details
-exports.details = async (req, res) =>{
+exports.details = async (req, res) => {
     await client.connect();
-    const filteredDocs = await collection.findOne({'username' : req.session.user.username});
+    const filteredDocs = await collection.findOne({ 'username': req.session.user.username });
     client.close();
     res.render('details', {
-        title: `${filteredDocs.username}'s Details`,
+        title: filteredDocs.username + "'s Details",
         person: filteredDocs
     });
 }
